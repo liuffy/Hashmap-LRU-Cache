0d057e95b66cbe88e318d4a65cc0c600f6b77371
Sophia Liu <sliu3@wellesley.edu>
2017-03-23 22:56:24 -0700

    Hashing scheme has been devised

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,13 +4,15 @@ end
 
 class Array
   def hash
-    almost = self.join(",").unpack("B*")
-    almost[0].to_i ^ 5
+    binary_array = self.join(",").unpack("B*")
+    binary_array[0].to_i ^ 5
   end
 end
 
 class String
   def hash
+    binary_array = self.unpack("B*")
+    binary_array[0].to_i ^ 5
   end
 end
 
@@ -18,6 +20,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_array = self.keys.map{|key| [key,self[key]]} #convert hash to an array with tuples
+    binary_array = hash_array.sort.join("").unpack("B*")
+    binary_array[0].to_i ^ 5
   end
 end
