0199b981ea832646534e8a1a6a97cf115ae1e548
Sophia Liu <sliu3@wellesley.edu>
2017-03-24 09:01:32 -0700

    Hash map complete

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,6 +2,8 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,18 +12,45 @@ class HashMap
   end
 
   def include?(key)
+   bucket(key).include?(key)
   end
 
+# set, insert a new link with the key and value into the correct bucket.
+# You can use your LinkedList#append method.) If the key already exists,
+# you will need to update the existing link.
   def set(key, val)
+    resize! if @count == num_buckets
+
+    if include?(key)
+      bucket(key).update(key,val)
+    else 
+      bucket(key).append(key, val)
+      @count += 1
+    end 
   end
 
+#To get, check whether the linked list contains the key you're looking up
   def get(key)
+    bucket(key).each{ |link| return link.val if link.key == key }
   end
 
+#To delete, remove the link corresponding to that key from the linked list
   def delete(key)
+    bucket(key).each do |link|
+      if link.key == key
+        link.remove
+        @count -= 1
+        return link.val
+      end 
+    end 
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |link|
+        yield [link.key, link.val]
+      end 
+    end 
   end
 
   # uncomment when you have Enumerable included
@@ -42,9 +71,17 @@ class HashMap
   end
 
   def resize!
+    old_store = @store
+    @count = 0 
+    @store = Array.new(num_buckets * 2) {LinkedList.new}
+
+    old_store.each do |bucket|
+      bucket.each{ |link| set(link.key, link.val)}
+    end 
   end
 
   def bucket(key)
+    @store[key.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `key`
   end
 end
