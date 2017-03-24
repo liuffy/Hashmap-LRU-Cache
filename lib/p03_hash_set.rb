65ac29a954daf01f79f4c397022f65d730ed1cc1
Sophia Liu <sliu3@wellesley.edu>
2017-03-23 23:16:22 -0700

    Hash set complete

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,27 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key.hash)
+    self[key.hash] << key
+    @count += 1
+    resize! if @count > num_buckets
+
+    key
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return false unless include?(key)
+    self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +37,10 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0 
+    @store = Array.new(num_buckets * 2) {Array.new}
+
+    old_store.flatten.each{ |num| insert(num) }
   end
 end
